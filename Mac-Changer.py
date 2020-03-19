15bd0cf49d766923c422d0de2c0e6f8d24add841
H3I53NB3RG <58789050+H3I53NB3RG@users.noreply.github.com>
2020-03-19 18:00:07 +0000

    Update Mac-Changer.py

--- Mac-Changer.py
+++ Mac-Changer.py
@@ -10,40 +10,50 @@ f.close()
 interface = input("[+] Interface You Want To Change Its Mac > ")
 
 if interface:
-	ifconfig_output = subprocess.check_output(["ifconfig", interface])
-	current_mac = re.search("\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(ifconfig_output))
-	
-	if current_mac:
-		print("[+] Your Current Mac Address is " + current_mac.group(0))
-	else:
-		import sys
-		print("[-] This Interface Has No Mac Address")
-		sys.exit(1)
 
-	new_mac = input("[+] New Mac Address > ")
-	
-	if new_mac:
-	
-		print("[+] Changing Mac Address To " + new_mac)
+	ifconfig = subprocess.run(["ip","link","show"], stdout=subprocess.PIPE)
+	text = ifconfig.stdout.decode("UTF-8")
+	result = text.find(": " + interface + ":")
 
-		subprocess.call(["ifconfig", interface, "down"])
-		subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-		subprocess.call(["ifconfig", interface, "up"])
-
+	if (result != -1):
 		ifconfig_output = subprocess.check_output(["ifconfig", interface])
 		current_mac = re.search("\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(ifconfig_output))
+	
+		if current_mac:
+			print("[+] Your Current Mac Address is " + current_mac.group(0))
+		else:
+			import sys
+			print("[-] This Interface Has No Mac Address")
+			sys.exit(1)
 
-		if new_mac == current_mac.group(0):
-			print("[+] Your New Mac Address For " + interface + " Is " + new_mac)
-		else:	
-			print("[-] Failed To Change Mac Address")
+		new_mac = input("[+] New Mac Address > ")
+		
+		if new_mac:
 
+			if re.search("\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(new_mac)) and len(new_mac) == 17:
+			
+				print("[+] Changing Mac Address To " + new_mac)
 
+				subprocess.call(["ifconfig", interface, "down"])
+				subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+				subprocess.call(["ifconfig", interface, "up"])
+
+				ifconfig_output = subprocess.check_output(["ifconfig", interface])
+				current_mac = re.search("\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(ifconfig_output))
+
+				if new_mac == current_mac.group(0):
+					print("[+] Your New Mac Address For " + interface + " Is " + new_mac)
+				else:	
+					print("[-] Failed To Change Mac Address")
+				
+			else:
+				print("[-] Invalid Mac Address")
+
+		else:
+			print("[-] Please Enter a New Mac")
 	else:
-		print("[-] Please Enter a New Mac")
+		print("[-] Invalid Interface")
 else:
 	print("[-] Please Enter Your Interface")
-
-
 
 
